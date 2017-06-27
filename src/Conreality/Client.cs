997d47d114703270c6fc09922d31e2f326d08f78
Arto Bendiken <arto@conreality.org>
2017-06-27 23:39:30 +0200

    Defined the Client class and assembly metadata.

---
+++ src/Conreality/Client.cs
@@ -0,0 +1,16 @@
+/* This is free and unencumbered software released into the public domain. */
+
+using System;
+using System.Data;
+
+namespace Conreality {
+
+  /// <summary>Conreality client.</summary>
+  public class Client {
+
+    /// <summary>Constructor.</summary>
+    public Client() {
+      throw new NotImplementedException("TODO");
+    }
+  }
+}
---
+++ src/Conreality/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+/* This is free and unencumbered software released into the public domain. */
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Conreality SDK for .NET")]
+[assembly: AssemblyDescription("Conreality Software Development Kit (SDK) for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Conreality.org")]
+[assembly: AssemblyProduct("Conreality")]
+[assembly: AssemblyCopyright("Public Domain")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: Guid("76b8e74c-ed39-464b-b340-c29cf3a323d9")]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
+[assembly: InternalsVisibleTo("Conreality.Test")]
