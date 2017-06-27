fa88c566a596c29534db88d6bc53b75a4847bbbd
Arto Bendiken <arto@conreality.org>
2017-06-28 00:32:53 +0200

    Ensured that Client implements IDisposable.

--- src/Conreality/Client.cs
+++ src/Conreality/Client.cs
@@ -3,14 +3,42 @@
 using System;
 using System.Data;
 
+using Npgsql;
+
 namespace Conreality {
 
   /// <summary>Conreality client.</summary>
-  public class Client {
+  public class Client : IDisposable {
+    private bool disposed = false;
+    private NpgsqlConnection connection;
 
     /// <summary>Constructor.</summary>
     public Client() {
-      throw new NotImplementedException("TODO");
+      // TODO
+    }
+
+    /// <summary></summary>
+    public void Dispose() {
+      this.Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    /// <summary></summary>
+    protected virtual void Dispose(bool disposing) {
+      if (!this.disposed) {
+        if (disposing) {
+          if (this.connection != null) {
+            this.connection.Dispose();
+            this.connection = null;
+          }
+        }
+        this.disposed = true;
+      }
+    }
+
+    /// <summary>Destructor.</summary>
+    ~Client() {
+      this.Dispose(false);
     }
   }
 }
